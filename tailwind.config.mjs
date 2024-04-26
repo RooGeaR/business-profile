76c2d50887fd694071852de5139e960b64225b8c
Roger Acosta <roger@mailvibes.io>
2024-04-25 21:00:10 -0600

    Added custom colors. Header menu as a list and blur on scroll functionality. Image rounded

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				primary1: "var(--color-primary-1)",
+				secondary: "var(--color-secondary)",
+				icon: "var(--color-icon)",
+				accent: {
+					light: "var(--color-accent-light)",
+					dark: "var(--color-accent-dark)"
+				}
+			}
+		},
 	},
 	plugins: [],
 }
